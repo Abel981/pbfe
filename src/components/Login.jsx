500abd0089c705ca9e7b9d2d871b40eb297dff8b
Abel981 <104688633+Abel981@users.noreply.github.com>
2023-08-12 23:00:34 -0700

    Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,176 @@ import AuthContext  from './shared/AuthContext'
 const Login = () => {
   const {login,isLoading} = useContext(AuthContext)
 
-    const {
+    const {import React, { useContext } from "react";
+import { useForm, Controller, FormProvider } from "react-hook-form";
+import Input from "@mui/joy/Input";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+import { LoadingButton } from "@mui/lab";
+import AuthContext from "./shared/AuthContext";
+import {
+  Avatar,
+  Box,
+  FormControlLabel,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Link } from "react-router-dom";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  username: z.string().min(3),
+  password: z.string().min(4),
+});
+
+const Login = () => {
+  const { login, isLoading } = useContext(AuthContext);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    const formData = new FormData();
+    formData.append("loginData", JSON.stringify(data));
+    return new Promise((resolve, reject) => {
+      login(data)
+        .then(() => {
+          toast.success("successfully logedin");
+          resolve();
+        })
+        .catch(() => {
+          toast.error("something went wrong");
+          reject();
+        });
+
+      // .then((res) =>{
+
+      //   console.log(res)
+      // resolve();
+      // }
+      // )
+      // .catch(() => {
+
+      //   console.log('error')
+      // reject()
+      // }
+      // );
+    });
+  };
+
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+
+  return (
+    <>
+      <Toaster />
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Agent Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit(onSubmit)}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                autoFocus
+                {...register("username", { required: true })}
+                error={errors.username}
+                helperText={errors.username?.message}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                {...register("password", { required: true })}
+                error={errors.password}
+                helperText={errors.password?.message}
+              />
+              <LoadingButton
+                type="submit"
+                color="secondary"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Login
+              </LoadingButton>
+              <Grid container>
+                <Grid item xs></Grid>
+                <Grid item>
+                  <Link to="/" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Login;
         register,
         handleSubmit,
         watch,
